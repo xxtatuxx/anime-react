3aaa133057f192fc59154bf50e7e9e53f937e847
xxtatuxx <abdogames9967@gmail.com>
2025-12-09 22:04:54 +0200

    Modify Vite config for base path and output directory
    Updated base path and output directory for build.

--- public/Front-end-react/vite.config.js
+++ public/Front-end-react/vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Front-end-react/dist/',
+  base: '/Front-end-react/',      // روابط تبدأ من هنا
   build: {
-    outDir: 'dist',
+    outDir: 'public/Front-end-react', // البناء يذهب مباشرة داخل public/Front-end-react
     assetsDir: 'assets',
+    emptyOutDir: true,                 // يمسح المجلد قبل البناء
   },
 })
